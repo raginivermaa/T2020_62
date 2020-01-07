bd006ac77b6eaf7f11ead9ce36584d953c9048fc
Ragini Verma <ragini.vermaa@gmail.com>
2020-01-07 14:09:44 +0800

    accepts parameters

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import app
 import requests
 from flask import render_template
+from flask import request
 import json
 
 @app.route('/')
@@ -14,26 +15,37 @@ headers_dict = {'identity' : 'T58', 'token': '57861dcd-3ed9-4647-9cb1-fbeac1d10e
 
 @app.route('/id')
 def get_id():
-    userName = 'marytan'
+    #userName = 'marytan'
+    userName = request.args.get('username', type=str)
+    if userName == None:
+        return ''
     response = requests.get('http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/customers/'
                             + userName, headers = headers_dict)
     print(response)
+    if response.status_code != 200:
+        return ''
     id_json = response.json()
     customerId = id_json['customerId']
     return str(id_json)
 
 @app.route('/details')
 def get_details():
-    customerId = '2'
+    #customerId = '2'
+    customerId = request.args.get('id', type=str)
+    if id == None:
+        return ''
     response = requests.get('http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/customers/'
                             + customerId + '/details', headers = headers_dict)
     print(response)
+    if response.status_code != 200:
+        return ''
     details_json = response.json()
     return str(details_json)
 
 @app.route('/deposit_accounts')
 def get_deposit_accounts():
-    customerId = '2'
+    #customerId = '2'
+    customerId = request.args.get('id', type=str)
     response = requests.get('http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/accounts/deposit/'
                             + customerId, headers = headers_dict)
     print(response)
@@ -42,7 +54,8 @@ def get_deposit_accounts():
 
 @app.route('/transactions')
 def get_transactions():
-    deposit_account = '79'
+    #deposit_account = '79'
+    deposit_account = request.args.get('account', type=str)
     from_date = '01-01-2018'
     to_date = '01-30-2020'
     response = requests.get(' http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/transactions/'
@@ -61,7 +74,8 @@ def get_transactions():
 
 @app.route('/balance')
 def get_balance():
-    deposit_account = '79'
+    deposit_account = request.args.get('account', type=str)
+    #deposit_account = '79'
     month = '0'
     year = '2019'
     response = requests.get('http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/accounts/deposit/'
@@ -78,7 +92,8 @@ def get_marketing():
 
 @app.route('/message')
 def get_marketing_message():
-    message_id = '2'
+    #message_id = '2'
+    message_id = request.args.get('msg', type=str)
     response = requests.get('http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/marketing/' + message_id,
                             headers = headers_dict)
     marketing_message = response.json()
@@ -86,7 +101,9 @@ def get_marketing_message():
 
 @app.route('/personal')
 def get_personal_message():
-    customerId = '2'
+    #customerId = '2'
+    customerId = request.args.get('id', type=str)
+
     response = requests.get('http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/message/' + customerId,
                             headers = headers_dict)
     message = response.json()
